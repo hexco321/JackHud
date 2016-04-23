ab36deecf482595c4712de12185895aaafd70b9a
BangL <henno.rickowski@gmail.com>
2016-04-23 03:53:15 +0200

    enhanced assault banner: endless wave fix

--- JackHUD/Lua/LocalizationManager_ext.lua
+++ JackHUD/Lua/LocalizationManager_ext.lua
@@ -13,24 +13,30 @@ function LocalizationManager:text(string_id, ...)
 end
 
 function LocalizationManager:hud_assault_enhanced()
-	if managers.groupai:state():get_hunt_mode() or not JackHUD:GetOption("enable_enhanced_assault_banner") then
+	if not JackHUD:GetOption("enable_enhanced_assault_banner") then
 		return self:text("hud_assault_assault")
 	else
-		local finaltext = "Assault Phase: " .. managers.groupai:state()._task_data.assault.phase
-		local spawns = managers.groupai:state():_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.force_pool) * managers.groupai:state():_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.force_pool_balance_mul)
-		if spawns >= 0 and JackHUD:GetOption("enhanced_assault_spawns") then
-			finaltext = finaltext .. " /// Spawns Left: " .. string.format("%d", spawns - managers.groupai:state()._task_data.assault.force_spawned)
-		end
-		local atime = managers.groupai:state()._task_data.assault.phase_end_t + math.lerp(managers.groupai:state():_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_min), managers.groupai:state():_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_max), math.random()) * managers.groupai:state():_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.sustain_duration_balance_mul) + tweak_data.group_ai.besiege.assault.fade_duration * 2
-		if JackHUD:GetOption("enhanced_assault_time") then
-			if atime < 0 then
-				finaltext = finaltext .. " /// OVERDUE"
-			elseif atime > 0 then
-				finaltext = finaltext .. " /// Time Left: " .. string.format("%.2f", atime + 350 - managers.groupai:state()._t)
+		local groupaistate = managers.groupai:state()
+		local finaltext = "Assault Phase: "
+		if groupaistate:get_hunt_mode() then
+			finaltext = finaltext .. "endless"
+		else
+			finaltext = finaltext .. groupaistate._task_data.assault.phase
+			local spawns = groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.force_pool) * groupaistate:_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.force_pool_balance_mul)
+			if spawns >= 0 and JackHUD:GetOption("enhanced_assault_spawns") then
+				finaltext = finaltext .. " /// Spawns Left: " .. string.format("%d", spawns - groupaistate._task_data.assault.force_spawned)
+			end
+			if JackHUD:GetOption("enhanced_assault_time") then
+				local atime = groupaistate._task_data.assault.phase_end_t + math.lerp(groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_min), groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_max), math.random()) * groupaistate:_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.sustain_duration_balance_mul) + tweak_data.group_ai.besiege.assault.fade_duration * 2
+				if atime < 0 then
+					finaltext = finaltext .. " /// OVERDUE"
+				elseif atime > 0 then
+					finaltext = finaltext .. " /// Time Left: " .. string.format("%.2f", atime + 350 - groupaistate._t)
+				end
 			end
 		end
 		if JackHUD:GetOption("enhanced_assault_count") then
-			finaltext = finaltext .. " /// Wave: " .. string.format("%d", managers.groupai:state()._wave_counter)
+			finaltext = finaltext .. " /// Wave: " .. string.format("%d", groupaistate._wave_counter or 0)
 		end
 		return finaltext
 	end
